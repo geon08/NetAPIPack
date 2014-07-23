31e2e084242508aa096e3dd9291a5e74ce540ad7
Blue_Ninja <geon08@daum.net>
2014-07-23 22:58:33 +0900

    Added handle

---
+++ NetAPIPack/NetAPIPack/Windows/Windows.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace NetAPIPack.Windows
+{
+    public class Windows
+    {
+        
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    }
+}
